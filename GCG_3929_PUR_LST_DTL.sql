1ab7e793fe0a253a29d76b51dab634c52059c9cc
Service Web <sw@gssmail.com>
2024-03-11 08:54:40 -0500

    Commit changes in C:\Websites\Git\emarlow\3929\GCG_3929_PUR_LST_DTL.sql

---
+++ GCG_3929_PUR_LST_DTL.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "GCG_3929_Pur_Lst_Dtl"(
+ "BIRUNID" BIGINT,
+ "Parent" CHAR(20),
+ "Component" CHAR(20),
+ "Description" CHAR(40),
+ "Qty_Each" NUMERIC(12,4),
+ "Qty_Total" NUMERIC(12,4),
+ "UM" CHAR(2),
+ "Comp_ID" CHAR(50),
+ "MFG_Part" CHAR(50),
+ "MFG_Name" CHAR(50),
+ "Sort_Code" CHAR(50));
