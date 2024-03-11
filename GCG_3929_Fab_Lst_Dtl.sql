dd43447613f7393ed1cbb391b3e740e1fd5c5608
Service Web <sw@gssmail.com>
2024-03-11 08:54:39 -0500

    Commit changes in C:\Websites\Git\emarlow\3929\GCG_3929_Fab_Lst_Dtl.sql

---
+++ GCG_3929_Fab_Lst_Dtl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_3929_Fab_Lst_Dtl"(
+ "BIRUNID" BIGINT,
+ "Program" CHAR(50),
+ "Top_Level_Parent" CHAR(20),
+ "Part" CHAR(20),
+ "Description" CHAR(40),
+ "Qty" NUMERIC(12,4),
+ "Shear_Size" CHAR(50),
+ "Material" CHAR(50),
+ "Department" CHAR(50)
+ "Rev" CHAR(20));
