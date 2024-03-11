7816a50ec931814ec3dbf3918f1e7433645704b5
Service Web <sw@gssmail.com>
2024-03-11 08:54:40 -0500

    Commit changes in C:\Websites\Git\emarlow\3929\GCG_3929_STD_BOM_Hdr.sql

---
+++ GCG_3929_STD_BOM_Hdr.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3929_STD_BOM_Hdr"(
+ "BIRUNID" BIGINT,
+ "JOB" CHAR(6),
+ "Suffix" CHAR(3),
+ "Parent" CHAR(20),
+ "Description" CHAR(40),
+ "BOM_Checked" CHAR(40),
+ "Qty" NUMERIC(12,4)
+ "Rev" CHAR(20));
