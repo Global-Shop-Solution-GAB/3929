ffc254587df182084dc0be09f947031325620404
Service Web <sw@gssmail.com>
2024-03-11 08:54:39 -0500

    Commit changes in C:\Websites\Git\emarlow\3929\GCG_3929_ENG_REL_HDR.sql

---
+++ GCG_3929_ENG_REL_HDR.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "GCG_3929_Eng_Rel_Hdr"(
+ "BIRUNID" BIGINT,
+ "Part" CHAR(20),
+ "Order_NO" CHAR(7),
+ "Order_Line" CHAR(4),
+ "Description" CHAR(40),
+ "Issue_BY" CHAR(30),
+ "Date_Released" DATE,
+ "Exception_Flag" CHAR(3));
