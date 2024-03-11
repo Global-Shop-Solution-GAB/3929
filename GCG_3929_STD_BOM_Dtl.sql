19b2b488b3a2da23f22edab1e272106c14eb3b99
Service Web <sw@gssmail.com>
2024-03-11 08:54:40 -0500

    Commit changes in C:\Websites\Git\emarlow\3929\GCG_3929_STD_BOM_Dtl.sql

---
+++ GCG_3929_STD_BOM_Dtl.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "GCG_3929_STD_BOM_Dtl"(
+ "BIRUNID" BIGINT,
+ "Parent" CHAR(20),
+ "Item_No" CHAR(8),
+ "Component" CHAR(20),
+ "Description" CHAR(40),
+ "Qty" NUMERIC(12,4),
+ "UM" CHAR(2),
+ "Shear_Size" CHAR(50),
+ "Material" CHAR(50),
+ "Program_NO" CHAR(50),
+ "Department" CHAR(50)
+ "Rev" CHAR(20));
