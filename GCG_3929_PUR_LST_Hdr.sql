d7ee1873e1007d9d7e98610964a98ae5bcec858c
Service Web <sw@gssmail.com>
2024-03-11 08:54:40 -0500

    Commit changes in C:\Websites\Git\emarlow\3929\GCG_3929_PUR_LST_Hdr.sql

---
+++ GCG_3929_PUR_LST_Hdr.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "GCG_3929_Pur_Lst_Hdr"(
+ "BIRUNID" BIGINT,
+ "Parent" CHAR(20),
+ "Order_NO" CHAR(7),
+ "Order_Line" CHAR(4),
+ "Ord_Qty" NUMERIC(12,4),
+ "RPM" CHAR(50),
+ "Description" CHAR(40),
+ "Ship_Date" DATE,
+ "Extra_Text_1" CHAR(50),
+ "Extra_Text_2" CHAR(50));
